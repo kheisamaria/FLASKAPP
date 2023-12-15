b3061cc52fd40ca97799b269f8f491e2ecf6b749
keisamae <kheisa.selma@cit.edu>
2023-12-15 23:41:32 +0800

    mijo home

--- expense/src/pages/Home.js
+++ expense/src/pages/Home.js
@@ -1,13 +1,50 @@
 // import "./App.css";
-
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import NavBar from "../components/NavBar";
 import Footer from "../components/Footer";
 import { useUserContext } from "../UserContext.js";
+import axios from "axios";
 
 function Home() {
   const { user } = useUserContext();
-  console.log(user);
+  console.log(user); //delete after sa home
+  const [transactions, setTransactions] = useState([]);
+  const [savings, setSavings] = useState([]);
+
+  useEffect(() => {
+    fetchTransactions();
+    fetchSavings();
+  }, [user.user_id]);
+
+  const fetchTransactions = async () => {
+    console.log(user.user_id);
+    try {
+      const result = await axios.get(
+        `http://localhost:5000/transactions/user/${user.user_id}`
+      );
+      setTransactions(result.data.length);
+      console.log(transactions);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const fetchSavings = async () => {
+    console.log(user.user_id);
+    try {
+      const result = await axios.get(
+        `http://localhost:5000/savings/user/${user.user_id}`
+      );
+      if (result.data.length === 0) {
+        setSavings("0.00");
+        console.log(savings);
+        return;
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <div className="bg-blue-950 h-screen w-screen">
       <NavBar />
@@ -16,7 +53,8 @@ function Home() {
         <div className="w-[550px] h-[550px] rounded-full bg-yellow-500 shadow-2xl border-6 border-blue-950 flex justify-center items-center">
           <div className="h-full w-full flex flex-col justify-center items-center">
             <div className=" h-20 w-full flex flex-row items-center justify-center font-bold text-7xl gap-x-1">
-              <span className="text-3xl">₱</span>58,000.00
+              <span className="text-3xl">₱</span>
+              {user.balance}
             </div>
             <div>Current Balance</div>
           </div>
@@ -25,7 +63,8 @@ function Home() {
           <Link to="/savings" className="h-full w-full">
             <div className="bg-yellow-500 h-32 w-full rounded-2xl shadow-2xl border-6 border-blue-950 flex items-center justify-center flex-col hover:bg-yellow-400">
               <div className=" h-12 w-full flex flex-row items-center justify-center font-bold text-4xl gap-1">
-                <span className="text-2xl">₱</span>45,000.00
+                <span className="text-2xl">₱</span>
+                {savings}
               </div>
               <div className="text-sm">Total Savings</div>
             </div>
@@ -41,7 +80,7 @@ function Home() {
           <Link to="/transactions" className="h-full w-full">
             <div className="bg-yellow-500 h-32 w-full rounded-2xl shadow-2xl border-6 border-blue-950 flex items-center justify-center flex-col hover:bg-yellow-400">
               <div className=" h-12 w-full flex flex-row items-center justify-center font-bold text-4xl gap-1">
-                206
+                {transactions}
               </div>
               <div className="text-sm">Number of Transactions</div>
             </div>

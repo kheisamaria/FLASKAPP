268605d117c479192aae7f78eb815290a53aaca1
Alicetair Cyric Toyoca <alestaircyrilculanag.coyoca@cit.edu>
2023-12-16 17:35:03 +0800

    asdcascd

--- moneytracker.sql
+++ moneytracker.sql
@@ -212,7 +212,20 @@ FOR EACH ROW
 BEGIN
     DECLARE user_balance DECIMAL(10, 2);
     SELECT balance INTO user_balance FROM users WHERE user_id = NEW.user_id;
-    UPDATE users SET balance = user_balance - NEW.amount WHERE user_id = NEW.user_id;
+
+    -- Check if the savings amount was updated
+    IF OLD IS NOT NULL AND NEW.amount != OLD.amount THEN
+        -- If the new amount is smaller, add the difference to the balance
+        IF NEW.amount < OLD.amount THEN
+            UPDATE users SET balance = user_balance + (OLD.amount - NEW.amount) WHERE user_id = NEW.user_id;
+        -- If the new amount is larger, subtract the difference from the balance
+        ELSE
+            UPDATE users SET balance = user_balance - (NEW.amount - OLD.amount) WHERE user_id = NEW.user_id;
+        END IF;
+    -- If it's a new savings entry, subtract the amount from the balance
+    ELSE
+        UPDATE users SET balance = user_balance - NEW.amount WHERE user_id = NEW.user_id;
+    END IF;
 END;
 //
 DELIMITER ;
@@ -305,7 +318,7 @@ FOR EACH ROW
 BEGIN
     DECLARE user_balance DECIMAL(10, 2);
     SELECT balance INTO user_balance FROM users WHERE user_id = NEW.user_id;
-    UPDATE users SET balance = user_balance + NEW.amount WHERE user_id = NEW.user_id;
+    UPDATE users SET balance = user_balance - NEW.amount WHERE user_id = NEW.user_id;
 END;
 //
 DELIMITER ;
